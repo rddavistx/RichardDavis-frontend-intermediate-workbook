31b7cb155b0a27c58f725a4fa5858fb76be40068
Richard Davis <laura@Lauras-MacBook-Pro.local>
2016-04-04 18:24:51 -0500

    08towers-of-hanoi

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -1,5 +1,40 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-}
+$(document).ready(function () {
+    //moving the block
+    var block = null;
+    $("[data-stack]").click(function () {
+        if (block === null) {
+            block = $(this).children().last().detach();
+        } else {
+            if (isLegal(block, $(this))) {
+                $(this).append(block);
+                block = null;
+            }
+            if (checkForWin()) {
+                $('#announce-game-won').text('You Won!');
+            }
+        }
+
+    });
+
+    function isLegal($stack) {
+        if ($stack.children().length === 0 || ($stack.children().last().attr('data-block')) > ($(block).attr('data-block'))) {
+            return true
+        }
+        return false;
+    };
+
+    function checkForWin() {
+        var gameWon = false;
+        $('[data-stack]').each(function () {
+            if ($(this).data('stack') !== 1 && $(this).children().length === 4) {
+                gameWon = true;
+            }
+        });
+        return gameWon;
+    }
+
+
+
+});
